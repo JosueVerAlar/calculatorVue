763653ad1b18945ac14f7e3d91b82973dd47c00b
Josué Vergara Alarcón <96144916+JosueVerAlar@users.noreply.github.com>
2024-07-05 00:28:36 -0600

    adding visibility button

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyComponent />
+  <HelloWorld />
 </template>
 
 <script>
+import MyComponent from './components/MyComponent.vue';
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MyComponent
   }
 }
 </script>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="my-component">
+        <h1 v-if="isVisible"> {{ message }}</h1>
+        <button v-on:click="_$event => isVisible = !isVisible">Ver mensaje</button> <!-- Botón usando eventos -->
+        <button v-on:click="toggleVisibility">Ver mensaje</button> <!-- Botón usando métodos -->
+        <input v-model="message"> <!-- Cambia el texto según lo que escribas -->
+        <button v-on:click="sayHello">Alerta de mensaje</button> <!-- Botón usando métodos -->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MyComponent',
+        data() {
+            return {
+                message: 'Hello with vue!',
+                isVisible: true
+            }
+        },
+        methods: {
+            toggleVisibility() {
+                this.isVisible = !this.isVisible;
+            },
+            sayHello () {
+                alert('Hello!!!')
+            }
+        }
+    }
+</script>
